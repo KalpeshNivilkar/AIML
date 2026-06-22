56455fed159ff2a7a07ea62a299def1cddd5508f
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-06-22 15:30:15 +0530

    add code

---
+++ 5_conditional_statement.py
@@ -0,0 +1,45 @@
+# grade stedents based on a marks
+'''marks = int(input("enter the marks: "))
+if marks >= 75 and marks <= 100:
+    print("grade A")
+elif marks >= 55 and marks <= 74:
+    print("grade B")
+elif marks >= 35 and marks <= 54:
+    print("grade C")
+else:
+    print("fail")'''
+
+# WAP to check if a number entered by the user is odd or even.
+'''num = int(input("enter any number: "))
+def is_num_even_odd(num):
+    if num % 2 == 0:
+        print("The",num,"is even")
+    else:
+        print("the",num,"is odd")
+is_num_even_odd(num)'''
+
+# WAP to find the greatest of 3 numbers entered by the user.
+'''num1 = int(input("enter  num 1:"))
+num2 = int(input("enter  num 2:"))
+num3 = int(input("enter  num 3:"))
+
+if num1 > num2 and num1 > num3:
+    print(num1,"is greater")
+elif num2 > num1 and num2 > num3:
+    print(num2,"is greater")
+else:
+    print(num3,"is greater")'''
+
+# WAP to c
+
+# A
+# heck
+# p
+# if a number is a multiple of 7 or not.
+
+num = int(input("enter the number: "))
+if num % 7 == 0:
+    print(num,"is divisible by 7")
+else:
+    print(num,"is not divisible by 7")
+    
