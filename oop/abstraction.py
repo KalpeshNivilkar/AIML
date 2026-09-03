fed86882d37980301b3c08f538bdb326e9917d7a
kalpesh nivilkar <178734425+KalpeshNivilkar@users.noreply.github.com>
2026-09-03 23:59:52 +0530

    add prcatice code inside the abtract file

--- oop/abstraction.py
+++ oop/abstraction.py
@@ -1,6 +1,7 @@
-from abc import ABC, abstractmethod
+'''from abc import ABC, abstractmethod
 
 class Animal(ABC):
+    
     @abstractmethod
     def make_sound(self):
         pass
@@ -17,4 +18,35 @@ l1 = cow()
 l1.make_sound()
 
 l2 = Lion()
-l2.make_sound()
+l2.make_sound()'''
+
+from abc import ABC, abstractmethod
+
+class Employee(ABC):
+    @abstractmethod
+    def calculate_salary(self):
+        pass
+
+class Intern(Employee):
+    def calculate_salary(self):
+        print("salary: 20_000")
+
+class FullTimeEmployee(Employee):
+    def calculate_salary(self):
+        print("salary: 50_000")
+
+class ContractEmployee(Employee):
+    def calculate_salary(self):
+        print("salary: 30_000")
+
+c1 = ContractEmployee()
+c1.calculate_salary()
+
+    
+    
+
+
+
+
+
+
