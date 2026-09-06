c109ea8d2d3fc8f570da01e38b6533fc46ea7499
kalpesh nivilkar <178734425+KalpeshNivilkar@users.noreply.github.com>
2026-09-06 23:12:28 +0530

    add file properties code

---
+++ File_handling/file_properties.py
@@ -0,0 +1,7 @@
+# this is file properties tutorials
+f = open("sample.txt","r")
+print("File name:",f.name)
+print("file mode:", f.mode)
+print("Is file closed", f.closed)
+f.close()
+print("Is file closed:", f.closed)
