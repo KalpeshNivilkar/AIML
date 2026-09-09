fc1b56a1bfdedd4ff51d12e973647e7bcd7ff0e0
kalpesh nivilkar <178734425+KalpeshNivilkar@users.noreply.github.com>
2026-09-09 18:11:52 +0530

    add json code

--- Exception_handling/example.py
+++ Exception_handling/example.py
@@ -1,4 +1,4 @@
-# first example
+'''# first example
 try:
     n = int(input("enter the value of n:"))
     ans = 10/ n
@@ -13,3 +13,51 @@ else:
 
 finally:
     print("program is end...")
+
+
+    '''
+
+# Take two numbers from the user.
+# Divide the first number by the second.
+# Handle ValueError if the user enters something that isn't a number.
+# Handle ZeroDivisionError if the second number is 0.
+# Print "Division successful" when there is no error.
+
+'''try:
+    num1 = int(input("enter num1: "))
+    num2 = int(input("enter num2: "))
+    num3 = num1 / num2
+
+except ValueError:
+    print("you entered invalid value...")
+
+except ZeroDivisionError:
+    print("Divided by zero is not possible...")
+
+else:
+    print(f"num3 = {num3}")'''
+
+
+# Write a program that asks the user for a filename and tries to open it.
+
+# Requirements:
+
+# Ask the user for a filename.
+# Try to open the file in "r" mode.
+# Handle FileNotFoundError.
+# If the file exists, print its contents.
+# Use finally to print:
+# Program execution completed.
+import json
+
+try:
+    file_name = input("Enter a file name: ")
+    with open(file_name, "r") as f:
+        data = json.load(f)
+    print(data)
+
+except FileNotFoundError:
+    print("you have enter wrong file name")
+
+finally:
+    print("program executed successfully...")
